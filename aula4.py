06d1b7ddce79fcd5e30132346746c16e4a837341
Cyro Guimaraes <cyro809@gmail.com>
2016-05-14 19:54:53 -0300

    Indices das palavras (indentificados separados por um espaço). Adiciona textos

--- aula4.py
+++ aula4.py
@@ -1,26 +1,30 @@
 import re
 import nltk
 
-filename = "teste"
-content = open(filename, 'r').read()
-articles = re.sub('<.*?>', '##', content).split('##')
-new_articles = [article.strip() for article in articles if not article.strip() == '']
+from nltk.tokenize import WhitespaceTokenizer
 
-# for i in range(len(new_articles)):
-# 	f_name = 'article{0}.txt'.format(i)
-# 	f = open(f_name, 'w')
-# 	f.write(new_articles[i])
+
+def split_articles():
+	filename = "teste"
+	content = open(filename, 'r').read()
+	articles = re.sub('<.*?>', '##', content).split('##')
+	new_articles = [article.strip() for article in articles if not article.strip() == '']
+
+	for i in range(len(new_articles)):
+		f_name = 'article{0}.txt'.format(i)
+		f = open(f_name, 'w')
+		f.write(new_articles[i])
 
 
 filename = 'article1.txt'
-
-
 content = open(filename, 'r').read()
 
 sent_tokenizer=nltk.data.load('tokenizers/punkt/english.pickle')
 indexes = sent_tokenizer.span_tokenize(content) # Indices de sentenca
 
-word_indexes = nltk.pos_tag(content)
+word_tokenizer = nltk.tokenize.api.StringTokenizer()
+word_indexes = list(WhitespaceTokenizer().span_tokenize(content)) #indices de palavras
 
 
 print word_indexes
+print indexes
