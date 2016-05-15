92ed7b7d83133125facefbde6753b019d59b01a8
Cyro Guimaraes <cyro809@gmail.com>
2016-05-15 12:36:28 -0300

    Retorna indices das entidades nomeadas

--- aula4.py
+++ aula4.py
@@ -29,19 +29,20 @@ sent2 = nltk.pos_tag(sent1)
 sent3 =  nltk.ne_chunk(sent2, binary=True)
 named_entities = []
 
+
 # Utilizado para retornar entidades nomeadas (Precisa de ajustes)
 for i in range(len(sent3)):
     if "NE" in str(sent3[i]):
         named_entities.append(' '.join(token[0] for token in sent3[i].leaves()))
 
 
+word_indexes = []
+for token in named_entities:
+    index = content.find(token)
+    word_indexes.append((index, index+len(token)))
 
-# word_indexes = [list(regexp_span_tokenize(content, re.compile("^(?!{0}).*$".format(token)))) for token in named_entities]
-
-# print word_indexes
+print word_indexes
 print
 print indexes
 print
 print named_entities
-
-# A ideia eh buscar as entidades nomeadas e atraves do regex tokenize (ou algum outro) buscar no texto as posicoes de cada uma
