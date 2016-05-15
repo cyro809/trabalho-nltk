3d00a4d6fffc9968780a38d148871b77e84cea3e
Cyro Guimaraes <cyro809@gmail.com>
2016-05-15 01:16:01 -0300

    Utiliza função para entidades nomeadas de nome completo

--- aula4.py
+++ aula4.py
@@ -2,7 +2,29 @@ import re
 import nltk
 
 from nltk.tokenize import WhitespaceTokenizer
+from nltk import ne_chunk, pos_tag, word_tokenize
+from nltk.tree import Tree
 
+
+# Utilizado para retornar as entidades nomeadas de nome completo (ex: "Barack Obama" ao invés de "Barack" e "Obama")
+def get_continuous_chunks(text):
+    chunked = ne_chunk(pos_tag(word_tokenize(text)))
+    prev = None
+    continuous_chunk = []
+    current_chunk = []
+
+    for i in chunked:
+        if type(i) == Tree:
+            current_chunk.append(" ".join([token for token, pos in i.leaves()]))
+        elif current_chunk:
+            named_entity = " ".join(current_chunk)
+            if named_entity not in continuous_chunk:
+                continuous_chunk.append(named_entity)
+                current_chunk = []
+        else:
+            continue
+
+    return continuous_chunk
 
 def split_articles():
 	filename = "teste"
@@ -22,9 +44,20 @@ content = open(filename, 'r').read()
 sent_tokenizer=nltk.data.load('tokenizers/punkt/english.pickle')
 indexes = sent_tokenizer.span_tokenize(content) # Indices de sentenca
 
-word_tokenizer = nltk.tokenize.api.StringTokenizer()
-word_indexes = list(WhitespaceTokenizer().span_tokenize(content)) #indices de palavras
+sent1 = nltk.word_tokenize(content)
+sent2 = nltk.pos_tag(sent1)
+sent3 =  nltk.ne_chunk(sent2, binary=True)
+named_entities = []
 
-
-print word_indexes
+# Utilizado para retornar entidades nomeadas (Precisa de ajustes)
+for i in range(len(sent3)):
+    if "NE" in str(sent3[i]):
+        named_entities.append([token for token, pos in sent3[i].leaves()])
+        #print sent3[i].leaves()[0][0]
 print indexes
+
+#print get_continuous_chunks(content)
+
+print named_entities
+
+# A ideia é buscar as entidades nomeadas e através do regex tokenize (ou algum outro) buscar no texto as posições de cada uma
